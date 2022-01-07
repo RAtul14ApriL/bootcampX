b401864c100ff415d15456baa46f52466377dcc0
Md Hefzul Bari <ratul14april@gmail.com>
2022-01-06 19:49:42 -0600

    Sanitized input values to prevent SQL Injection

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,7 +1,8 @@
 const { Pool } = require('pg');
 
 const cohort = process.argv[2];
-const limit = process.argv[3];
+const limit = process.argv[3] || 5;
+const values = [`%${cohort}%`, limit];
 
 const pool = new Pool({
   user: 'labber',
@@ -14,10 +15,10 @@ pool.query(`
 SELECT students.id, students.name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = students.cohort_id
-WHERE cohorts.name LIKE '%${cohort}%'
+WHERE cohorts.name LIKE $1
 ORDER BY students.id
-LIMIT ${limit || 5};
-`)
+LIMIT $2;
+`, values)
 .then(res => {
   // console.log(res.rows);
   res.rows.forEach(user => {
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,5 +1,9 @@
 const { Pool } = require('pg');
 
+const input = process.argv[2];
+const cohort = `%${input}%` || 'JUL02';
+const values = [cohort];
+
 const pool = new Pool({
   user: 'labber',
   password: 'labber',
@@ -13,9 +17,9 @@ FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name LIKE $1
 ORDER BY teacher;
-`)
+`, values)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
